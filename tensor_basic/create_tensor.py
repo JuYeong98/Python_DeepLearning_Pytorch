9c3ce0e372bf9a4eef217474931337a285913de9
JuYeong98 <twinjuy@naver.com>
2023-01-16 21:40:29 +0900

    2nd commit

--- tensor_basic/create_tensor.py
+++ tensor_basic/create_tensor.py
@@ -4,6 +4,7 @@ print(torch.tensor([[1,2],[3,4]]))
 print(torch.tensor([[1,2],[3,4]] , device = 'cuda:0'))
 print(torch.tensor([[1,2],[3,4]], dtype = torch.float64))
 
+
 '''
 tensor([[1, 2],
         [3, 4]])
